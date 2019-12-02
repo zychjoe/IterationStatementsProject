f9d6a97c315656945ae384b85e50d6bbc9bceada
nate <nrice@truecoders.io>
2019-12-02 16:50:21 -0600

    init commit

---
+++ IterationStatements/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IterationStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO
+
+            //Create a List called "numbers"
+            //Create a variable of type int with an initializer of 0
+
+
+
+            // Create a do-while loop
+            {
+                // Increment your variable by 1
+                // Then add your variable to "numbers"
+            }
+            // While your variable is less than 100
+
+
+
+            // Create a while loop
+            // While your variable is less than 200
+            {
+                // Increment your variable by 1
+                // Then add your variable to "numbers"
+            }
+
+            Console.WriteLine("Increase:");
+
+            // Create a foreach loop
+            // Write your variable to the console
+
+            Console.WriteLine("");
+            Console.WriteLine("Decrease:");
+
+            // Create a for loop
+            // in your initializer set the value of i to 199
+            // in your conditional, as long as i is less than or equal to the length of "numbers"
+            // and as long as i is greater than or equal to 0
+            // Decrement i by 1
+            {
+                // Write to the console "numbers" at index i
+            }
+        }
+    }
+}
