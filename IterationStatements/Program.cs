31fc39fe47f4be208f2bf51f9515d05395f03cf4
mvdoyle <mdoyle@truecoders.io>
2020-06-24 10:47:36 -0500

    finished

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -11,31 +11,35 @@ namespace IterationStatements
 
             //Create a List called "numbers" - DONE
             var numbers = new List<int>();
-            
+
             //Create a variable of type int and initialize the variable with a value of 0
+            var init = 0;
 
-
-
-            // Create a do-while loop
+            do
             {
                 // Increment your variable by 1
-                // Then add your variable to "numbers"
-            }
-            // While your variable is less than 100
+                init++;
+                // Then add your variable to "numbers" - done
+                numbers.Add(init);
 
-
+            } while (init < 100);
 
             // Create a while loop
             // While your variable is less than 200
+            while (init < 200)
             {
-                // Increment your variable by 1
-                // Then add your variable to "numbers"
+                init++;
+                numbers.Add(init);
             }
 
             Console.WriteLine("Increase:");
 
             // Create a foreach loop
             // Write your variable to the console
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
 
             Console.WriteLine("");
             Console.WriteLine("Decrease:");
@@ -43,10 +47,12 @@ namespace IterationStatements
             // Create a for loop
             // in your initializer set the value of i to 199
             // in your conditional, as long as i is less than or equal to the length of "numbers"
-            // and as long as i is greater than or equal to 0
+            // AND as long as i is greater than or equal to 0
             // Decrement i by 1
+            for (int i = 199; i <= numbers.Count && i >= 0; i--)
             {
                 // Write to the console "numbers" at index i
+                Console.WriteLine(numbers[i]);
             }
         }
     }
