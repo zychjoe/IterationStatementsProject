7aa7ec41c9702332aa12287359bf780a338d0259
Michael Doyle <mdoyle@truecoders.io>
2020-10-30 09:53:23 -0500

    updated wording

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -58,7 +58,7 @@ namespace IterationStatements
             // Create a for loop - this will print the numbers in reverse order - from 200 to 1
                 // in your initializer set the value of i to 199
                 // in your conditional, as long as i is less than or equal to the amount of items in "numbers" - use (numbers.Count)
-                // and as long as i is greater than or equal to 0
+                // AND as long as i is greater than or equal to 0
                 // Decrement i by 1
 
             //start for loop here
